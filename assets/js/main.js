a2131224dd7d45691dfc4a5b9a715a8f4db2aed4
Geoffrey C. Hutchinson <geoffreyhutchinson018@gmail.com>
2020-12-03 18:43:58 +0000

    js code initially tweaked with a new function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,24 @@
-  const cards = document.querySelectorAll('.megaman-character');
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('text-theme'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MegaManMemoryGame(60, cards);
 
-  let cardFlipped = false;
-  let megaManTurn = false;
-  let characterOne, chracterTwo;
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('show');
+            game.startGame();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);
+        });
+    });
+}
 
-  function flipCard() {
-   if (megaManTurn) return;
-   if (this === characterOne) return;
-
-    this.classList.add('flip');
-
-    if (!cardFlipped) {
-      cardFlipped= true;
-      characterOne = this;
-      return;
-    }
-
-    chracterTwo = this;
-  
-    checkForMatch();
-  }
-
-  function checkForMatch() {
-    let characterMatch = characterOne.dataset.framework === chracterTwo.dataset.framework;
-    characterMatch ? disableCards() : unflipCards();
-  }
-
-  function disableCards() {
-    characterOne.removeEventListener('click', flipCard);
-    chracterTwo.removeEventListener('click', flipCard);
-
-   resetBoard();
-  }
-
-  function unflipCards() {
-    megaManTurn = true;
-
-    setTimeout(() => {
-      characterOne.classList.remove('flip');
-      chracterTwo.classList.remove('flip');
-
-     resetBoard();
-    }, 1500);
-  }
-
- function resetBoard() {
-   [cardFlipped, megaManTurn] = [false, false];
-   [characterOne, chracterTwo] = [null, null];
- }
-
-  (function shuffle() {
-   cards.forEach(card => {
-     let shufflingCard = Math.floor(Math.random() * 12);
-     card.style.order = shufflingCard;
-   });
- })();
-
-  cards.forEach(card => card.addEventListener('click', flipCard));
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
 
