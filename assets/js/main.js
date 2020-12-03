b46622022a981254ce3e451e1b06eca25cb60134
Geoffrey C. Hutchinson <geoffreyhutchinson018@gmail.com>
2020-12-03 19:13:44 +0000

    audio controller class added on main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,38 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('assets/soundtrack/titlescreen.mp3');
+        this.flipSound = new Audio('assets/soundtrack/pickacard.mp3');
+        this.matchSound = new Audio('assets/soundtrack/rightcards.mp3');
+        this.winSound = new Audio('assets/soundtrack/victory.mp3');
+        this.gameOverSound = new Audio('assets/soundtrack/gameover.mp3');
+        this.bgMusic.volume = 0.4;
+        this.flipSound.volume = 0.1;
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    win() {
+        this.stopMusic();
+        this.winSound.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+}
+
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('text-theme'));
     let cards = Array.from(document.getElementsByClassName('card'));
@@ -21,4 +56,8 @@ if(document.readyState === 'loading') {
 } else {
     ready();
 }
+
+
+
+
 
